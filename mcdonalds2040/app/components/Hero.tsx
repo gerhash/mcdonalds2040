2e6f4edd27f4026f67e01b8ec0ecec24c0b063ec
G#sh <62940515+gerhash@users.noreply.github.com>
2024-01-31 11:39:58 +0100

    hero update
    impostate immagini secondarie con rotazioni ecc

--- mcdonalds2040/app/components/Hero.tsx
+++ mcdonalds2040/app/components/Hero.tsx
@@ -1,20 +1,25 @@
+'use client' 
 import React from 'react'
 import { logo, wrap, burger, cola,fries} from '../images/index'
 import Image from 'next/image';
 
 const Hero: React.FC = () => {
+ 
     return (
       <div className='flex flex-col content-center items-center '>
-          <div className='h-[850px] h-[200px] flex justify-center items-center'>
+          <div className='h-[900px] flex justify-center items-center'>
             <Image src={logo} alt="McDonald's" className='w-[220px] h-[200px]'/> 
           </div>
-          <div>
-
-
-            <Image src={wrap} alt="McWrap" className='w-[220px] h-[200px]'/> 
-            <Image src={burger} alt="Crispy" className='w-[220px] h-[200px]'/> 
-            <Image src={cola} alt="CocaCola" className='w-[220px] h-[200px]'/> 
-            <Image src={fries} alt="Stick" className='w-[220px] h-[200px]'/> 
+          <div className='flex flex-col w-full items-center'>
+              <div className='flex '>
+                <Image src={wrap} alt="McWrap" className='w-[330px] h-[300px] origin-center rotate-30 mr-[700px] -translate-y-[200px]'/> 
+                <Image src={fries} alt="Stick" className='w-[220px] h-[200px] origin-center rotate-[-20deg] '/> 
+              </div>
+              <div className='flex  '>
+                <Image src={burger} alt="Crispy" className='w-[220px] h-[200px] parallax-image'/> 
+                <Image src={cola} alt="CocaCola" className='w-[220px] h-[200px] parallax-image'/> 
+              </div>
+       
           </div>
       </div>
     );
