0688e579b0f9096ec35d9a982234b38e6afa8f65
G#sh <62940515+gerhash@users.noreply.github.com>
2024-01-31 15:08:52 +0100

    Offer
    Sezione Offer completata con un carousel

--- mcdonalds2040/app/components/Offer.tsx
+++ mcdonalds2040/app/components/Offer.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useRef }  from 'react'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { slider1,slider2,slider3 } from '../images';
+import Image from 'next/image';
 
+
+  
 const Offer = () => {
-  return (
-    <div className='bg-white'>Offer</div>
-  )
-}
+        const images = [
+         slider1,
+          slider2,
+          slider3,
+        ];
+      
+        const sliderRef = useRef<Slider>(null);
+
+        const settings = {
+          dots: true,
+          infinite: true,
+          speed: 200,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        };
+
+        return (
+          <div className="w-full flex flex-col justify-center items-center">
+            <div className="w-[950px] h-[650px] overflow-hidden border-4 border-[#886AB5] rounded-2xl  ">
+            <Slider {...settings}  ref={sliderRef} className="object-cover  ">
+                {images.map((image, index) => (
+                <div key={index} className="">
+                    <Image src={image} alt={`slide-${index + 1}`}  />
+                </div>
+                ))}
+            </Slider>
+            </div>
+            <div className='text-white mt-10 flex gap-10 '>
+                <div className="p-4 bg-[#886AB5] rounded-full" onClick={() => sliderRef.current?.slickPrev()}>
+                    <svg className="w-16 h-16 transform " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
+                    </svg>
+
+                </div>
+                <div className="p-4 bg-[#886AB5] rounded-full"  onClick={() => sliderRef.current?.slickNext()}>
+                    <svg className="w-16 h-16" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/>
+                    </svg>
+                </div>
+            </div>
+
+          </div>
+        );
+      };
 
 export default Offer
--- mcdonalds2040/app/images/index.js
+++ mcdonalds2040/app/images/index.js
@@ -3,6 +3,9 @@ import wrap from './1.png'
 import burger from './2.png'
 import cola from './3.png'
 import fries from './4.png'
+import slider1 from './slider1.png'
+import slider2 from './slider2.png'
+import slider3 from './slider3.png'
 
 
-export { logo,wrap,burger,cola,fries};
+export { logo,wrap,burger,cola,fries,slider1,slider2,slider3};
