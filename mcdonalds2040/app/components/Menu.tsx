4decdf4ee6105aa0d1db04658e48ad55e0f5af5d
G#sh <62940515+gerhash@users.noreply.github.com>
2024-01-31 15:54:41 +0100

    footer
    completato footer, da mettere LINK e una parte del vortice

--- mcdonalds2040/app/components/Menu.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { category_menu } from '../images'
-import Image from 'next/image'
-
--- mcdonalds2040/app/page.tsx
+++ mcdonalds2040/app/page.tsx
@@ -6,6 +6,9 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { log } from 'console';
 import Offer from "./components/Offer";
 import Menu from './components/Menu';
+import { IoLogoBehance,IoLogoInstagram,IoLogoLinkedin,IoLogoGithub,IoLogoDribbble } from "react-icons/io5";
+
+
 
 export default function Home() {
   return (
@@ -35,10 +38,34 @@ export default function Home() {
                 <ParallaxLayer offset={1.9} speed={3} className='flex justify-center p-[50px]'>
                   <h1 className='text-transparent text-[100px] font-semibold bg-clip-text bg-gradient-to-r from-[#886AB5] to-[#C1FF72] translate-y-[1000px] '>Menu</h1>
                 </ParallaxLayer>
+
                 <ParallaxLayer offset={2} speed={1.3} className='flex justify-center p-[50px]'>
-                    <Menu />
+                <h1 className='text-transparent text-[70px] font-semibold bg-clip-text bg-[#886AB5] translate-x-[180px]'>Per Mangiare Gratis</h1>
+                </ParallaxLayer>
+                <ParallaxLayer offset={2} speed={0.5} className='flex justify-center p-[50px]'>
+                <h1 className='text-transparent text-[100px] font-semibold bg-clip-text bg-white -translate-x-[200px] translate-y-[200px]'>Seguimi Su</h1>
+                </ParallaxLayer>
+                <ParallaxLayer offset={2.5} speed={0.8} className='flex justify-center p-[50px]'>
+                    <div className='flex justify-center gap-4'>
+                        <IoLogoLinkedin size={120} color="white"/>
+                        <IoLogoGithub size={120} color="white"/>
+                        <IoLogoInstagram size={120} color="white"/>
+                        <IoLogoBehance size={120} color="white"/>
+                        <IoLogoDribbble size={120} color="white"/>
+                    </div>
+                </ParallaxLayer>
+
+                <ParallaxLayer offset={2.8} speed={0.5} className='flex justify-center p-[50px]'>
+                <h1 className='text-white text-[70px] font-semibold'>Gerardo D'Agostino</h1>
+                </ParallaxLayer>
+                <ParallaxLayer offset={2.9} speed={1} className='flex justify-center p-[50px]'>
+                <h1 className='text-white text-[120px] font-semibold'>Other Works</h1>
+                </ParallaxLayer>
+                <ParallaxLayer offset={2.9} speed={0} className='text-white flex justify-center items-center'>
+                          VORTICE
                 </ParallaxLayer>
             </Parallax>
+           
   </main>
   );
 }
