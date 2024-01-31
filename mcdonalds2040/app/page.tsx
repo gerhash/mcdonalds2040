56a4c2a8268a2f80f940413a369ddf57aa93e3a2
G#sh <62940515+gerhash@users.noreply.github.com>
2024-01-31 12:43:52 +0100

    HERO PARALLAX
    sezione hero parallax completa

--- mcdonalds2040/app/page.tsx
+++ mcdonalds2040/app/page.tsx
@@ -1,10 +1,11 @@
 import Hero from "./components/Hero";
+import Herotest from "./components/Herotest";
 
 export default function Home() {
   return (
    <main>
 
-    <Hero />
+    <Herotest />
 
   </main>
   );
