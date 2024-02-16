15c569b7b0a6bcb43e7e74d1ca2488d8270a8be6
G#sh <62940515+gerhash@users.noreply.github.com>
2024-02-16 17:32:56 +0100

    menu

--- mcdonalds2040/app/components/Menu.tsx
+++ mcdonalds2040/app/components/Menu.tsx
@@ -16,29 +16,57 @@ const products = [
     {
         category:"Bevande",
         items:[
-            {id:7, title:"CocaCola-G4®",motto:"Gusto CocaCola, G-4 di potenza!",desc:"Alla spina dispponibile nei formati 0,25L, 0,4L, 0,5L, 1,5L, 12,2L.",path:""},
-            {id:8, title:"MoonWater®",motto:"Not a Simple Water!",desc:"Acqua speciale dal principale fiume lunare, disponbiile solo 0,5L.",path:""},
-            {id:9, title:"Oxygen",motto:"Freschezza, naturalezza, nostalgia!",desc:"Ormai da anni non è più essenziale, ma è un piacere respirarlo di nuovo, bibita all'Ossigeno puro, formato da 0,5L.",path:""},
-            {id:10, title:"Antidote Rad",motto:"Scudo della pelle, sapore di stelle",desc:"Bevanda fresca estiva, sapore tropicale marziano, perfetta per curare radiazioni e ustioni, formato da 0,3L.",path:""},
-            {id:11, title:"SHIELD",motto:"Ricordi quel gioco la la...",desc:"50% scudo dopo averla bevuto, sapore blueberry, ottima consistenza, consigliato, formato soltanto 0,5L in giara di vetro",path:""},
+            {id:7, title:"CocaCola-G4®",motto:"Gusto CocaCola, G-4 di potenza!",desc:"Alla spina dispponibile nei formati 0.25L, 0.5L, 1.5L, 12.0L",path:""},
+            {id:8, title:"MoonWater®",motto:"Not a Simple Water!",desc:"Acqua speciale dal principale fiume lunare, disponbiile solo 0.5L.",path:""},
+            {id:9, title:"Oxygen",motto:"Freschezza, naturalezza, nostalgia!",desc:"Ormai da anni non è più essenziale, ma è un piacere respirarlo di nuovo, bibita all'Ossigeno puro, formato da 0.5L.",path:""},
+            {id:10, title:"Antidote Rad",motto:"Scudo della pelle, sapore di stelle",desc:"Bevanda fresca estiva, sapore tropicale marziano, perfetta per curare radiazioni e ustioni, formato da 0.3L.",path:""},
+            {id:11, title:"SHIELD",motto:"Ricordi quel gioco la la...",desc:"50% scudo dopo averla bevuto, sapore blueberry, ottima consistenza, consigliato, formato soltanto 0.5L in giara di vetro",path:""},
 
         ]
     },
     {
         category:"Patatine",
-        items:[{}]
+        items:[
+            {id:12, title:"Patatine Mars® Regular",motto:"Classiche ed Inimitabili!",desc:"Perfette per uno Snack, da gustare da sole o accompagnate da una delle nostre salse. Patate tagliate interamente a Parallelepipedi!",path:""},
+            {id:13, title:"Patatine Mars® TITAN",motto:"Senza che menu Sarebbe?",desc:"Le riconosci subito, anche con tutti e 7 gli occhi chiusi! Sono Lunghe, dorate, Euclidiane, Quadridimensionali...goditele con tutte le salse! PORZIONI DA VERO TITANO***",path:""},
+            {id:14, title:"Le Ricche Mars® + MoonQueso®",motto:"Gli amanti del Queso Lunare sono serviti!",desc:"Le patatine di sempre più tutto il gusto del salsa calda MoonQueso®, lasciati tentare dalle golosissime!",path:""},
+            {id:15, title:"Le Ricche Mars® + MoonQueso® + Asteroids®",motto:"Asteroids® stanno bene su TUTTO!",desc:"Le patatine di sempre più tutto il gusto del salsa calda MoonQueso® + extra Asteroids®, lasciati tentare dalle golosissime!",path:""},
+        ]
     },
     {
         category:"Sfiziosità",
-        items:[{}]
+        items:[
+            {id:16, title:"Stranger McMars®",motto:"Con cosa sono fatti!(NESSUNO LO SA)",desc:"Tenere crocchette di ?????? impanato/a. Buonissime da sole, squisite con le salse, radiottive con le bibite, sono un Grande Classico a cui è difficile resistere. Le puoi gustare come vuoi, da solo o in compagnia, anche se probabilmente le mangerai tutte solo.",path:""},
+            {id:17, title:"Panzarotti Ripieni Vintage®",motto:"Un classico Sud Italiano, da oltre 100 anni",desc:"Un cuore caldo e filante per farti vivere i ricordi terrestri, 100% nostalgici, pomodoro e mozzarella sono un piacere per il palato.",path:""},
+            {id:18, title:"McSnack®",motto:"Uranio Tascabile",desc:"Ormai la merenda dei lavoratori, dalla scoperta dell'uranio commestibile è una grarantita carica di energie e fonte disumana di proteine, ;D",path:""},
+            {id:18, title:"Asteroids®",motto:"Impossibile Resistere!",desc:"Con il loro gusto unico, la loro croccantezza e la qualità delle Rocce Spaziali, le Asteroids ti conquisteranno",path:""},
+
+        ]
     },
     {
         category:"Dessert & Gelati",
-        items:[{}]
+        items:[
+            {id:19, title:"McFlurry® Uranio",motto:"Un vortice pericoloso",desc:"Il gusto unico di McFlurry® in una nuova versione super golosa, con Uranio e granella croccante. Tieniti pronto ad un gelato NUCLEARE.",path:""},
+            {id:20, title:"McFlurry® Water",motto:"Vintage e Gustoso",desc:"Da quando l'acqua è stata sostituita come fonte di vita primaria...nessuno ha più voluto saperne, ma quanto è buona l'ACQUA? Disponibile McFlurry® gusto ACQUA pura di sorgente!",path:""},
+            {id:21, title:"Ice Cream Silicium",motto:"Chi resiste al Silicio",desc:"Gelato al Silicio fatto con 100% Silicio, combinazione golosa tra O2 e Silicio ti farà sciogliere ad ogni cucchiaiata.",path:""},
+            {id:22, title:"Ice Cream Asteroids®",motto:"Pioggia di Meteoriti!",desc:"Classico Gelato con tempesta di Meteore! Ti cadranno i dent per la croccantezza delle Rocce Spaziali...",path:""},
+            {id:23, title:"Cono",motto:"Semplicemente Irresistibile",desc:"Il Cono McDonalds è così da sempre: semplice,cremoso e irresistibile. Una golosa spirale di gelato al fiordilatte, fatto con latte intero 100% marziano, su una cialda croccante, per chi ha sempre voglia di gelato.",path:""},
+            {id:24, title:"Donut Asteroids®",motto:"Il più goloso",desc:"Un dolce donut con una Pioggia di Rocce Lunari Asteroids® sopra, irresistibili",path:""},
+            {id:25, title:"Donut Fragolona",motto:"La Felicità",desc:"Un dolce donut con una glassatura di zucchero rosa e Fragolona sopra.",path:""},
+
+        ]
     },
     {
         category:"Salse",
-        items:[{}]
+        items:[
+            {id:26, title:"Ketchup",motto:"Dolcezza inconfondibile",desc:"Alleato indiscusso delle nostre patatine, il nostro ketchup accompagnerà tutta la bontà dei tuoi Menu Preferiti.",path:""},
+            {id:27, title:"Maionese",motto:"Sapore Cremoso",desc:"Delicata, Cremosa, Irresistibile. La nostra maionese divide tutti: c'è chi li adora con le patatine, chi con gli Strangers McMars®, chi con tutti i nostri altri prodotti",path:""},
+            {id:28, title:"Salsa Crispy McBacon® Vintage",motto:"Il gusto del panino nostalgico più amato di sempre",desc:"Era leggenda, finalmente è tornata la salsa più amata dai Crispy lover e non solo, cosa aspetti? Corri a provarla!",path:""},
+            {id:29, title:"Agrosalatadolcepiccante",motto:"Armonia di sapori",desc:"Dolce, salata e piccante allo stesso momento, nata per i più confusi dell'universo, ti stupirà quando la assaggerai con le Patatine, l'hai già provata?",path:""},
+            {id:30, title:"Nuclear",motto:"Intensa e mortale",desc:"Vuoi dare un tocco di sapore extra e rischio al tuo piacere? Non perdere la nostra salsa Nuclear a base di Uranio!",path:""},
+            {id:31, title:"Mexicana",motto:"¡Picante como la vida misma!",desc:"Nuestra salsa mexicana es como un mariachi en tu paladar: ¡te hace bailar, te hace sudar y te deja con ganas de más! Prepárate para una explosión de sabores que te hará exclamar '¡Ay, caramba!' en cada bocado. ¡Pero cuidado! Como un buen chisme, una vez que la pruebas, ¡no podrás mantenerla en secreto! ¡Que viva el sabor de México en cada salsa!",path:""},
+
+        ]
     }
 ]
 
@@ -85,7 +113,7 @@ const Menu = () => {
 
   return (
     <div className='flex justify-betweem w-full'>
-         <VisibilitySensor onChange={(isVisible) => setIsVisible(isVisible)}>
+         <VisibilitySensor onChange={(isVisible:any) => setIsVisible(isVisible)}>
         <div className='w-1/3  pl-[30px] pr-[10px] flex flex-col justify-center gap-4'>
             {products.map((product) => (
                 <animated.div style={fade} key={product.category} className='text-white cursor-pointer'  onClick={() => handleCategoryClick(product.category)}>
